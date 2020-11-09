ff0a754715da5410073c3d63302ac68ebaec8c2c
Anthony <apears313@gmail.com>
2020-11-08 20:52:15 -0500

    Formatted card and card-img and added transformation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,36 @@
-$(function() {
-    $("#login").click(function(){
-        $("#loginModal").modal("show");
-    });
-    $("#signup").click(function(){
-        $("#signupModal").modal("show");
-    });
+const recipeCards = $('.recipeCard').on('click',showRecipeModal);
+
+
+$('.login').on('click', function() {
+    $('#loginModal').modal('show');
 });
+
+$('.signup').on('click', function() {
+    $('#signupModal').modal('show');
+});
+
+function showRecipeModal() {
+    var recipeModal = $('#recipeModal');
+    var index = $('.recipeCard').index($(this));
+    var img = recipeCards[index].querySelector('img');
+    var modalImg = document.getElementById('img01');
+    var title = recipeCards[index].querySelector('div').textContent;
+    
+    // console.log(img.getAttribute('src'));
+    // console.log(img.getAttribute('alt'));
+    console.log(title);
+
+    modalImg.src = img.getAttribute('src');
+    modalImg.alt = img.getAttribute('alt');
+    //console.log(index);
+    
+    
+    //console.log(title);
+    recipeModal.modal('show');
+    $('.modal-title').text(title);
+}
+
+function pictureTransform() {
+
+}
+
